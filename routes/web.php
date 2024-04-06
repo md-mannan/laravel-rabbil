18c042bcdd100b65a6c3157cff0594cd74cbe51a
md-mannan <mdmannan580@gmail.com>
2024-04-06 11:09:17 +0300

    Routing with Controller & View

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
+//Routing with Controller & View
+
+
 Route::get('/', [SiteController::class, 'Home']);
 Route::get('/about', [SiteController::class, 'About']);
 Route::get('/contact', [SiteController::class, 'Contact']);
