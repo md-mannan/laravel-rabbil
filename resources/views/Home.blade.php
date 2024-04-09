4bb2a97880c8d7a117e8d89d52c82926cacf8585
md-mannan <mdmannan580@gmail.com>
2024-04-09 14:08:16 +0300

    Sub View

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -8,26 +8,39 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
-<body class="">
+<body style="background:#eee;">
     <div class="container ">
-        <h1 class="alert alert-success mt-5">ForEach loop in Blade view</h1>
-        <div class="card">
 
-            <!-- For loop with Blade View -->
+        <!-- Sub View -->
+        @include('sub-view.Menu')
 
-            <div class="card-body">
 
-                <!-- normal php in blade view -->
-                @php
-                $num1=10;
-                $num2=30;
-                $sum=$num1+$num2;
-                echo $sum;
+        <div style="min-height:80vh;background:#fff">
 
-                echo date();
-                @endphp
-            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         </div>
+
+
+
+
+
+
+
+        @include('sub-view.Footer')
 
 
     </div>
